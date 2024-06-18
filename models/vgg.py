7a58806fdd3543b37b218fc9fe6ab2d63fc14ec0
anshit0205 <anshitdas408@gmail.com>
2024-06-18 12:55:02 +0530

    Add files via upload

---
+++ models/vgg.py
@@ -0,0 +1,12 @@
+import tensorflow as tf
+from tensorflow.keras.applications.vgg16 import VGG16
+
+def VGG16_Avgpool(shape):
+    vgg = VGG16(input_shape=shape, weights='imagenet', include_top=False)
+    new_model = tf.keras.Sequential()
+    for layer in vgg.layers:
+        if isinstance(layer, tf.keras.layers.MaxPooling2D):
+            new_model.add(tf.keras.layers.AveragePooling2D())
+        else:
+            new_model.add(layer)
+    return new_model
